91d82943e40ca0dc34ce55458f1f63e0dfb0836e
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-06-20 20:21:43 +0200

    Changes 1

--- src/main/java/pl/javastart/match/Match.java
+++ src/main/java/pl/javastart/match/Match.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
+import pl.javastart.bet.Bet;
 
 import java.time.LocalDate;
 
